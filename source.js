f63b21c9200937576c7a5a8dc167c138eac90e7d
maxim-sid <maxim32ro@gmail.com>
2021-11-20 09:42:30 +0200

    forms update

--- source.js
+++ source.js
@@ -1,5 +1,36 @@
 "use strict"
 
+document.addEventListener('DOMContentLoaded', function (){
+  const form = document.getElementById('form_req');
+  form.addEventListener('submit', formSend);
+
+  async function formSend(e) {
+    e.preventDefault();
+      let error = 0;
+      var regEx = /^(?:\+38)?(0[5-9][0-9]\d{7})$/gm;
+      var regex = /^[а-яёА-ЯЁ a-zA-zЇІ]{2,60}$/;
+      let phoneNumber = document.getElementById("phone");
+      let nameVal = document.getElementById("fio");
+      if(!phoneNumber.value.match(regEx)){
+        error++;
+      }
+      if(!nameVal.value.match(regex)){
+        error++;
+      }
+    let formData = new FormData(form);
+
+    if (error === 0){
+      fetch('/', {
+        method: 'POST',
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+      }).then(() => alert('Форма успешно отправлена!'));
+    } else {
+      alert("Пожалуйста введите правильный номер телефона и/или ФИО");
+    }
+  }
+});
+
 function myFunction(a) {
     a.parentNode.getElementsByClassName("dropdown-content")[0].classList.toggle("show");
 }
